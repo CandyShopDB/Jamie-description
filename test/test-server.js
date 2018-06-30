1f9ef45ff7f5fa66542754478a290bd8d49aad3d
Eunbyul Cho <hello@eunbyulcho.me>
2018-06-30 14:53:36 -0700

    change connect

--- test/test-server.js
+++ test/test-server.js
@@ -24,6 +24,18 @@ describe('connect to pool', () => {
       database: 'jamie',
       port: '5432',
     });
+    pool.connect((err, client, release) => {
+      if (err) {
+        return console.error('Error acquiring client', err.stack)
+       }
+      client.query('SELECT NOW()', (err, result) => {
+        release()
+        if (err) {
+          return console.error('Error executing query', err.stack)
+        }
+        console.log(result.rows)
+      })
+    })
     pool.query('SELECT * FROM host where id = 1');
   });
 
