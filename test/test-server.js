aa022567eb5c2987f7e3e6426218b99ef682964a
Eunbyul Cho <hello@eunbyulcho.me>
2018-07-01 17:33:32 -0700

    )

--- test/test-server.js
+++ test/test-server.js
@@ -22,7 +22,7 @@ pool.query('DROP TABLE IF EXISTS house', (err, res) => {
   console.log('drop table house', err, res);
 //  pool.end();
 });
-pool.query('CREATE TABLE house (id serial PRIMARY KEY,property_type VARCHAR(500),title VARCHAR(500),location VARCHAR(500),num_guests smallint,num_beds smallint,num_views smallint,num_baths smallint,num_rooms smallint,studio  Boolean,score smallint,description_title VARCHAR(500),days_from_last_update INT,minimumstay smallint,checkin_start_time smallint,checkout_time smallint,description_comment VARCHAR(5000),host_id INT,highlights_id INT,cancellation_id INT,house_rules_id INT,amenities_id INT', (err, res) => {
+pool.query('CREATE TABLE house (id serial PRIMARY KEY, property_type VARCHAR(500), title VARCHAR(500), location VARCHAR(500), num_guests smallint, num_beds smallint, num_views smallint, num_baths smallint, num_rooms smallint, studio Boolean, score smallint, description_title VARCHAR(500), days_from_last_update INT, minimumstay smallint, checkin_start_time smallint, checkout_time smallint, description_comment VARCHAR(5000), host_id INT, highlights_id INT, cancellation_id INT, house_rules_id INT, amenities_id INT)', (err, res) => {
   console.log('create table house', err, res);
 //  pool.end();
 });
